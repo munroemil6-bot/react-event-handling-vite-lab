32ddafa7be7d70581492fc92e2150e04fbd428e0
mylesmunroe <munroemil6@gmail.com>
2026-05-01 22:52:48 +0300

    input design for passwordinput.jsx initialized as well as button in submit nutton.jsx

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,8 +1,14 @@
 // Code PasswordInput Component Here
 
 function PasswordInput (){
+      function handleChange() {
+    console.log("Entering password...");
+  }
+
     return(
-        <></>
+    <div>
+      <input type="password" placeholder="Enter password" onChange={handleChange}/>
+    </div>
     )
 }
 
--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,9 +1,25 @@
 // Code SubmitButton Component Here
 
 function SubmitButton (){
+
+      function handleEnter() {
+    console.log("Mouse Entering");
+  }
+
+  function handleLeave() {
+    console.log("Mouse Exiting");
+  }
+
     return(
-        <></>
-    )
+    <div>
+      <button 
+        onMouseEnter={handleEnter}
+        onMouseLeave={handleLeave}
+      >
+        Submit Password
+      </button>
+    </div>   
+     )
 }
 
 export default SubmitButton;
